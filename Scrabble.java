a810ee431741ade48d46492b9792468ca33b3cad
Madison Sayles <msayles123@gmail.com>
2021-04-03 15:10:20 -0400

    points,tiles update,end game,restart

--- Scrabble.java
+++ Scrabble.java
@@ -8,8 +8,8 @@ public class Scrabble{
 	private static Scrabble game;
 	private JFrame frame;	//main frame
 	private JFrame prompt;	//initial prompt window
+	private JFrame game_over;
 	private TextField people;
-	private TextField computers;
 	private JButton start;
 	private JLabel welcome;		//changes if user types invalid number of players
 	private BoardPanel board;
@@ -18,6 +18,16 @@ public class Scrabble{
 	private JButton player2[];
 	private JButton player3[];
 	private JButton player4[];
+	private int turn;			//indicates whose turn it is
+	private JButton end_turn;
+	private int player_points[];
+	private JLabel point_label[];
+	private int turn_points;
+	private JButton clicked;	//will refer to indexed tile
+	private int index;			//so can shift tiles over
+	private int replace;		//how many tiles to replace at end of turn
+	private JButton end_game;
+	private JButton restart;
 	
 	public static void main(String[] args){
 		game = new Scrabble();
@@ -29,6 +39,7 @@ public class Scrabble{
 		prompt = new JFrame("Prompt");
 		prompt.setSize(200,250);
 		
+		//welcome message
 		welcome = new JLabel("Welcome");
 		welcome.setHorizontalAlignment(JLabel.CENTER);
 		prompt.add(welcome);
@@ -36,7 +47,8 @@ public class Scrabble{
 		number_info.setHorizontalAlignment(JLabel.CENTER);
 		prompt.add(number_info);
 		
-		JLabel people_text = new JLabel("players (people)");
+		//request how many people are playing at that time
+		JLabel people_text = new JLabel("Number of Players");
 		people_text.setHorizontalAlignment(JLabel.CENTER);
 		prompt.add(people_text);
 		people = new TextField();
@@ -54,7 +66,7 @@ public class Scrabble{
 		
 	}
 	
-	char setPlayerTiles(){	//returns replacement tile
+	char setPlayerTiles(){	//returns valid tile
 		
 		Random rand = new Random();
 		int index;
@@ -98,6 +110,45 @@ public class Scrabble{
 		return ' ';
 	}
 	
+	int points(JButton tile){
+		switch(tile.getText().charAt(0)){	//given tile and returns points
+			case 'A': return 1;
+			case 'B': return 3;
+			case 'C': return 3;
+			case 'D': return 2;
+			case 'E': return 1;
+			case 'F': return 4;
+			case 'G': return 2;
+			case 'H': return 4;
+			case 'I': return 1;
+			case 'J': return 8;
+			case 'K': return 5;
+			case 'L': return 1;
+			case 'M': return 3;
+			case 'N': return 1;
+			case 'O': return 1;
+			case 'P': return 3;
+			case 'Q': return 10;
+			case 'R': return 1;
+			case 'S': return 1;
+			case 'T': return 1;
+			case 'U': return 1;
+			case 'V': return 4;
+			case 'W': return 4;
+			case 'X': return 8;
+			case 'Y': return 4;
+			case 'Z': return 10;
+			case ' ': return 0;
+		}
+		
+		return 0;
+	}
+	
+	void addToPoints(int num){
+		turn_points += num;
+	}
+	
+	
 	//inner class for event handling
 	private class ScrabbleHandler implements ActionListener{
 		public void actionPerformed(ActionEvent event){	//start button was clicked
@@ -112,12 +163,21 @@ public class Scrabble{
 				}else{	//valid number of players
 					prompt.setVisible(false);		//user can no longer see prompt window
 					
+					turn_points = 0;
+					player_points = new int[4];
+					for(int i = 0; i < total; i++){
+						player_points[i] = 0;
+					}
+					replace = 0;
+					ScrabbleHandler handler = new ScrabbleHandler();	//event handler
+					
 					frame = new JFrame("Scrabble");
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setSize(700,500);
+					frame.setSize(700,600);
 					frame.setLayout(new GridBagLayout());
 					GridBagConstraints c = new GridBagConstraints();
 					
+					//title at top
 					JLabel title = new JLabel("Scrabble");
 					title.setHorizontalAlignment(JLabel.CENTER);
 					c.fill = GridBagConstraints.HORIZONTAL;
@@ -130,6 +190,8 @@ public class Scrabble{
 					c.gridx = 0;
 					c.gridy = 1;
 					frame.add(board,c);
+					board.clicked = new JButton();
+					board.clicked = null;
 					
 					//States points per letters
 					JLabel points = new JLabel();
@@ -140,21 +202,43 @@ public class Scrabble{
 					c.anchor= GridBagConstraints.NORTHEAST;
 					frame.add(points,c);
 					
+					//button indicates whose turn it is and when clicked shifts to new player
+					turn = 1;
+					end_turn = new JButton("Player 1 End Turn");
+					c.gridx = 1;
+					c.gridy = 2;
+					frame.add(end_turn,c);
+					//event handler
+					end_turn.addActionListener(handler);
+					
+					end_game = new JButton("End Game");
+					c.gridx = 1;
+					c.gridy = 3;
+					frame.add(end_game,c);
+					end_game.addActionListener(handler);
+					
 					//second half of window (still in process)
 					JPanel right_side = new JPanel();
 					right_side.setLayout(new GridBagLayout());
 					GridBagConstraints d = new GridBagConstraints();
 					frame.add(right_side);
 					
+					point_label = new JLabel[total];
+					
 					//goes through for each player
 					for(int i = 0; i < total; i++){
 						int num = i + 1;
 						JLabel player_num = new JLabel();
 						player_num.setText("Player " + num);
-						//player_num.setHorizontalAlignment(JLabel.CENTER);
 						d.gridx = 2;
 						d.gridy = (3*i) + 1;
 						right_side.add(player_num,d);
+						
+						point_label[i] = new JLabel("Points: " + player_points[i]);
+						d.gridx = 4;
+						d.gridy = (3*i) + 1;
+						right_side.add(point_label[i],d);
+						
 						char letter;
 						//still need to set up tile arrays
 						for(int j = 0; j < 7; j++){
@@ -182,30 +266,164 @@ public class Scrabble{
 									letter = game.setPlayerTiles();
 									player1[j] = new JButton(String.valueOf(letter));
 									right_side.add(player1[j],d);
+									player1[j].addActionListener(handler);
 									break;
 								case 1:
 									letter = game.setPlayerTiles();
 									player2[j] = new JButton(String.valueOf(letter));
 									right_side.add(player2[j],d);
+									player2[j].addActionListener(handler);
 									break;
 								case 2:
 									letter = game.setPlayerTiles();
 									player3[j] = new JButton(String.valueOf(letter));
 									right_side.add(player3[j],d);
+									player3[j].addActionListener(handler);
 									break;
 								case 3:
 									letter = game.setPlayerTiles();
 									player4[j] = new JButton(String.valueOf(letter));
 									right_side.add(player4[j],d);
+									player4[j].addActionListener(handler);
 							}
+							
 						}
 						
 					}
 					
-				/*	JLabel temp = new JLabel("this is to show what happens");
-					frame.add(temp,BorderLayout.LINE_END);
-				*/	
+					turn = 1;	//start with player1
 					frame.setVisible(true);
+				}
+				
+			}else if(event.getSource() == end_turn){
+				//adds total points for that turn to players total and goes to next player
+				if(board.tw)
+					turn_points *= 3;
+				if(board.dw)
+					turn_points *= 2;
+				
+				player_points[turn-1] += turn_points;
+				point_label[turn-1].setText("Points: " + player_points[turn-1]);
+				
+				char letter;
+				
+				while(replace > 0){
+					switch(turn){
+						case 1:
+							letter = game.setPlayerTiles();
+							player1[7 - replace].setText(String.valueOf(letter));
+							break;
+						case 2:
+							letter = game.setPlayerTiles();
+							player2[7 - replace].setText(String.valueOf(letter));
+							break;
+						case 3:
+							letter = game.setPlayerTiles();
+							player3[7 - replace].setText(String.valueOf(letter));
+							break;
+						case 4:
+							letter = game.setPlayerTiles();
+							player4[7 - replace].setText(String.valueOf(letter));
+							break;
+					}
+					replace--;
+				}
+				
+				switch(turn){
+					case 1:
+						turn = 2;
+						end_turn.setText("Player 2 End Turn");
+						break;
+					case 2:
+						if(total < 3){
+							turn = 1;
+							end_turn.setText("Player 1 End Turn");
+						}else{
+							turn = 3;
+							end_turn.setText("Player 3 End Turn");
+						}
+						break;
+					case 3:
+						if(total < 4){
+							turn = 1;
+							end_turn.setText("Player 1 End Turn");
+						}else{
+							turn = 4;
+							end_turn.setText("Player 4 End Turn");
+						}
+						break;
+					case 4:
+						turn = 1;
+						end_turn.setText("Player 1 End Turn");
+						break;
+				}
+				turn_points = 0;
+				board.dw = false;
+				board.tw = false;
+				
+			}else if(event.getSource() == end_game){
+				frame.setVisible(false);
+				game_over = new JFrame("Game Over");
+				game_over.setSize(200,200);
+				game_over.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				
+				JLabel end1 = new JLabel("Game Ended");
+				end1.setHorizontalAlignment(JLabel.CENTER);
+				game_over.add(end1);
+				
+				int high_score = 0;
+				StringBuilder winner = new StringBuilder("Player 1");
+				for(int i = 0; i < total; i++){
+					if(player_points[i] > high_score){
+						high_score = player_points[i];
+						winner.replace(0,7,"Player " + (i+1));
+					}
+				}
+				
+				JLabel end2 = new JLabel("Winner");
+				end2.setHorizontalAlignment(JLabel.CENTER);
+				game_over.add(end2);
+				
+				JLabel end3 = new JLabel(String.valueOf(winner));
+				end3.setHorizontalAlignment(JLabel.CENTER);
+				game_over.add(end3);
+				
+				JLabel end4 = new JLabel(String.valueOf(high_score));
+				end4.setHorizontalAlignment(JLabel.CENTER);
+				game_over.add(end4);
+				
+				restart = new JButton("Restart Game");
+				game_over.add(restart);
+				ScrabbleHandler handler = new ScrabbleHandler();
+				restart.addActionListener(handler);
+				
+				game_over.setLayout(new GridLayout(5,1));
+				game_over.setVisible(true);
+				
+			}else if(event.getSource() == restart){
+				game = new Scrabble();
+			}else{	//if a player's tile is clicked
+				JButton temp[] = new JButton[7];
+				switch(turn){	//only valid it is their turn
+					case 1:
+						temp = player1;
+						break;
+					case 2:
+						temp = player2;
+						break;
+					case 3:
+						temp = player3;
+						break;
+					case 4:
+						temp = player4;
+						break;
+				}
+				for(int i = 0; i < 7; i++){
+					if(event.getSource() == temp[i]){
+						board.clicked = temp[i];
+						index = i;
+						i = 7;
+					}
 				}
 				
 			}
@@ -218,9 +436,15 @@ public class Scrabble{
 	
 		private JButton[][] tiles;	//board tiles
 		private int available[];	//bag of tiles
+		private JButton clicked;
+		private boolean dw;
+		private boolean tw;
 		
 		public BoardPanel(){
 			super();
+			
+			dw = false;
+			tw = false;
 			
 			//keep record of how many tiles "in bag"
 			available = new int[27];
@@ -461,8 +685,6 @@ public class Scrabble{
 				}
 			}
 			
-			//points per letter
-			
 		}
 		
 		//inner class for event handling
@@ -472,7 +694,53 @@ public class Scrabble{
 				for(int i = 0; i < 15; i++){
 					for(int j = 0; j < 15; j++){
 						if(event.getSource() == tiles[i][j]){
-							
+							//if a tile has been clicked add points and change board tile text
+							if(clicked != null){
+								if(tiles[i][j].getText() == "DL"){
+									game.turn_points += (game.points(clicked)*2);
+								}else if(tiles[i][j].getText() == "DW"){
+									dw = true;
+									game.turn_points += game.points(clicked);
+								}else if(tiles[i][j].getText() == "TL"){
+									game.turn_points += (game.points(clicked)*3);
+								}else if(tiles[i][j].getText() == "TW"){
+									tw = true;
+									game.turn_points += game.points(clicked);
+								}else{
+									game.turn_points += game.points(clicked);
+								}
+								tiles[i][j].setText(clicked.getText());
+								tiles[i][j].setBorder(BorderFactory.createLineBorder(Color.RED));	//draw border
+								clicked = null;
+								game.replace++;
+								
+								switch(game.turn){
+									case 1:
+										for(int s = game.index; s < 7 - game.replace; s++){
+											game.player1[s].setText(game.player1[s + 1].getText());
+										}
+										player1[7 - game.replace].setText(" ");
+										break;
+									case 2:
+										for(int s = game.index; s < 7 - game.replace; s++){
+											game.player2[s].setText(game.player2[s + 1].getText());
+										}
+										player2[7 - game.replace].setText(" ");
+										break;
+									case 3:
+										for(int s = game.index; s < 7 - game.replace; s++){
+											game.player3[s].setText(game.player3[s + 1].getText());
+										}
+										player3[7 - game.replace].setText(" ");
+										break;
+									case 4:
+										for(int s = game.index; s < 7 - game.replace; s++){
+											game.player4[s].setText(game.player4[s + 1].getText());
+										}
+										player4[7 - game.replace].setText(" ");
+										break;
+								}
+							}
 							
 							
 						}
